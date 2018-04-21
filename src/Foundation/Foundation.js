567ce91736c49a742f8b1812b1ce312a18b913ae
Keary Suska <>
2018-04-21 14:53:42 -0600

    * changed Foundation.js so imports before exporting so we can control the order of imports, which allows us to use it as a proper import point for Foundation objects

--- src/Foundation/Foundation.js
+++ src/Foundation/Foundation.js
@@ -2,21 +2,30 @@
  * Single-point import for Foundation framework
  **/
 
-// we have to import these since we use them
+import objj_msgSend, {objj_propGuard} from '../Objective-J';
+import CPObject from './CPObject';
 import CPString from './CPString';
+import CPMethodSignature from './CPMethodSignature';
+import CPInvocation from './CPInvocation';
+import CPException, {CPInvalidArgumentException} from './CPException';
+import CPDictionary from "./CPDictionary";
 
+/*
+ * Objective-J Runtime Exports
+*/
+
+export {objj_msgSend, objj_propGuard};
+
+/*
+ * Object Exports
+*/
+
+export {CPObject, CPString, CPException, CPInvalidArgumentException, CPMethodSignature, CPInvocation, CPDictionary};
+
+/*
+ * Selector Function Exports
+ */
 
 export const CPSelectorFromString = (selString) => selString.jsString();
 export const CPStringFromSelector = (selector) => new CPString(selector);
 
-
-/*
- * object exports
-*/
-
-export {default as CPObject} from './CPObject';
-export {CPString};
-export {default as CPException, CPInvalidArgumentException} from './CPException';
-export {default as CPMethodSignature} from "./CPMethodSignature";
-export {default as CPInvocation} from './CPInvocation';
-export {default as CPDictionary} from "./CPDictionary";
