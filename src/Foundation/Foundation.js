6f85b3598865e4055112c84a19c27a408b9f19ac
Keary Suska <>
2018-04-25 11:10:07 -0600

    * added import of Protocol class to Foundation; added grouping comments to imports

--- src/Foundation/Foundation.js
+++ src/Foundation/Foundation.js
@@ -2,7 +2,10 @@
  * Single-point import for Foundation framework
  **/
 
+// runtime imports
 import objj_msgSend, {objj_propGuard} from '../Objective-J';
+import Protocol from './Protocol';
+// object imports
 import CPObject from './CPObject';
 import CPString from './CPString';
 import CPMethodSignature from './CPMethodSignature';
@@ -15,6 +18,7 @@ import CPDictionary from "./CPDictionary";
 */
 
 export {objj_msgSend, objj_propGuard};
+export {Protocol};
 
 /*
  * Object Exports
