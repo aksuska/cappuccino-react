e20436e792eecabd4da7a03c5c6fa23703796540
Keary Suska <>
2018-06-04 15:26:40 -0600

    * changed objj_throw_arg() calls to instead use CRException directly since we can now that we have a circular dependency solution

--- src/Foundation/CPDictionary.js
+++ src/Foundation/CPDictionary.js
@@ -25,14 +25,14 @@ class CRDictionary extends CRObject {
 	$setObjectForKey(object, key) {
 		// null key is exception
 		if (key === null)
-			objj_throw_arg("-[%@ setObject:forKey:]: key cannot be null", this.className);
+			objj_initialize(CRException).raise_format_(CRInvalidArgumentException, new CRString("-[%@ setObject:forKey:]: key cannot be null"), this.className);
 		else if (object == null)
-			objj_throw_arg("-[%@ setObject:forKey:]: object cannot be null (key: %@)", this.className, key);
+			objj_initialize(CRException).raise_format_(CRInvalidArgumentException, new CRString("-[%@ setObject:forKey:]: object cannot be null (key: %@)"), this.className, key);
 
 		// we need to copy key, but there is a chance we'll get null
 		const ourKey = objj_msgSend(key, 'copyWithZone:', null);
 		if (ourKey === null)
-			objj_throw_arg("-[%@ setObject:forKey:]: key cannot be null", this.className);
+			objj_initialize(CRException).raise_format_(CRInvalidArgumentException, new CRString("-[%@ setObject:forKey:]: key cannot be null"), this.className);
 		else
 			this.$jsMap.set(ourKey, object);
 	}
